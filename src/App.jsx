658768e5382875979589dd5b0167928403958dca
Rafael Sena <rafaelmurilobs@gmail.com>
2026-03-29 13:37:59 -0300

    Projeto Pronto

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/Addtask";
+
+function App() {
+   const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=10");
+      const data = await response.json();
+      setTasks(data);
+    };
+    // SE QUISER, VOCÊ PODE CHAMAR UMA API PARA PEGAR AS TAREFAS
+    //fetchTasks();
+  }, []);
+
+
+  function onTaskClick(taskId){
+    const newTasks = tasks.map((task) => {
+      if(task.id === taskId){
+        return {
+          ...task,
+          isCompleted: !task.isCompleted
+        }
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+
+  function onDeleteTaskClick(taskId){
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
+
+  function onAddTaskSubmit(title, description){
+    const newTask = {
+      id: tasks.length + 1,
+      title,
+      description,
+      isCompleted: false
+    }
+    setTasks([...tasks, newTask]);
+  }
+
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-6">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+        <Tasks 
+        tasks={tasks} 
+        onTaskClick={onTaskClick} 
+        onDeleteTaskClick={onDeleteTaskClick} 
+        />
+      </div>
+    </div>
+  )
+}
+
+export default App
